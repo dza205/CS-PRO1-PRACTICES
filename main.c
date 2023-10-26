9378564212aa4a8eea27d452e30866d2416f9499
communist-funny-man <danidezaprieto@gmail.com>
2023-10-26 21:55:39 +0200

    Added exercises this time around in order

--- main.c
+++ main.c
@@ -3,58 +3,59 @@
 #include <stdlib.h>
 #include <math.h>
 int ex1(){
-    int dd = 0;
-    int mm = 0;
-    printf("Introduzca la fecha en la que se sitúa dentro del año en formato dia y mes\n");
-    scanf("%d %d",&dd,&mm);
-    int situation = (30.4166666667*(mm-1)+dd);
-    printf("Le quedan %d dias del año\n",situation);
+    printf("Input a whole number\n");
+    int num = 0;
+    char t[] = "true";
+    char f[] = "false";
+    scanf("%i");
+    printf("The number is %s",(num<0)?t:f);
 }
 int ex2(){
- int ndd;
- int nmm;
- int nyy;
- int birthdd;
- int birthmm;
- int birthyy;
- int life = ((nyy - birthyy)*365+(30.4166666667*(nmm-birthmm))+(ndd-birthdd)); 
- printf("Please input your birthday in day month year format day month year\n");
- scanf("%d %d %d",&birthdd,&birthmm,&birthyy);
- printf("Please input today's date in the same format as your birthday\n");
- scanf("%d %d %d",&ndd,&nmm,&nyy);
- printf("you are %lf days old",life);
+    printf("Please input a whole number\n");
+    int num = 0;
+    scanf("%i",&num);
+    if((num % 2) != 0){
+        printf("The number is even");
+    }
+    else{
+        printf("The number is odd");
+    }
 
 }
-
 int ex3(){
-float salary;
-int kids;
-printf("What is your salary?\n");
-scanf("%f",&salary);
-printf("How many children do you have?\n");
-scanf("%d",&kids);
-float irpfcalc = (salary*0.15);
-printf("The IRPF you have to pay is %f \n",(irpfcalc*(kids*0.1)));
+    printf("Please input two whole numbers\n");
+    int n1,n2;
+    scanf("%i %i",&n1,&n2);
+    if((n1%n2) != 0){
+        printf("The first number is divisible by the other");
+    }
+    else{
+        printf("The first number is not divisible by the other");
+    }
 }
-
 int ex4(){
-    int a,b,c;
-    printf("Please input the three coeficients of a second grade equation\n");
-    scanf("%d %d %d",&a,&b,&c);
-    double x1 = (((-b)+sqrt((b^2)-(4*a*c)))/2);
-    double x2 = (((-b)-sqrt((b^2)-(4*a*c)))/2);
-    printf("The two possible solutions are %lf and %lf",x1,x2);
-
+    printf("Please input 3 numbers\n");
+    int n1,n2,n3;
+    scanf("%i %i %i",&n1,&n2,&n3);
+    int largerbetweeen2 =((n1>n2)?n1:n2);
+    int finalnum = ((largerbetweeen2>n3)?largerbetweeen2:n3);
+    printf("%i is the largest number",finalnum);
 }
-
 int ex5(){
-    
+    printf("Please input 3 numbers\n"); 
+    int n1,n2,n3; // declare array of 3 numbers
+    scanf("%i %i %i",&n1,&n2,&n3);
+    unsigned int largerbetweeen2 =((n1>n2)?n1:n2);
+    unsigned int finalnum = ((largerbetweeen2>n3)?largerbetweeen2:n3);
+    unsigned int smallest = (n1 + n2 +n3) - largerbetweeen2 - finalnum; // unsigned moment
+    printf("The numbers in order are %i %i %i",smallest,largerbetweeen2,finalnum);
 }
 
-int main(int, char**){
-int exnum;
-printf("Input the number of the exercise you want to execute\n");
-scanf("%d",&exnum);
+int main(){
+    printf("Input the number of the exercise you'd like to execute\n");
+    int exnum = 0;
+    scanf("%i",&exnum);
+
 switch(exnum)
 {
     case(1):
@@ -68,6 +69,9 @@ switch(exnum)
     break;
     case(4):
     ex4();
+    break;
+    case(5):
+    ex5();
     break;
 
 }
